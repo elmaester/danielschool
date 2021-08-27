37ea2ab369052e054f89e97104ebd0a9ffac7358
elmaester <olegmorket@gmail.com>
2021-08-27 14:14:09 +0300

    fix navigation

--- src/text/footerText.js
+++ src/text/footerText.js
@@ -1,100 +1,110 @@
 export const navigation = [
   {
     heading: {
-      uk: 'Про школу',
-      en: 'About the school',
+      uk: "Про школу",
+      en: "About the school",
     },
     items: [
       {
-        uk: 'Про нас',
-        en: 'About us',
-        link: 'about',
+        uk: "Про нас",
+        en: "About us",
+        link: "about",
       },
       {
-        uk: 'Вакансії',
-        en: 'Jobs',
-        link: 'jobs',
+        uk: "Вакансії",
+        en: "Jobs",
+        link: "jobs",
       },
     ],
   },
   {
     heading: {
-      uk: 'Англійська',
-      en: 'English',
+      uk: "Англійська",
+      en: "English",
     },
     items: [
       {
-        uk: 'Індивідуальні уроки',
-        en: 'Personal lessons',
-        link: 'personal-lessons',
+        uk: "Індивідуальні уроки",
+        en: "Personal lessons",
+        link: "personal-lessons",
       },
       {
-        uk: 'Групові заняття',
-        en: 'Group classes',
-        link: 'group-classes',
+        uk: "Дорослі групи",
+        en: "Adult groups",
+        link: "adults-group",
       },
       {
-        uk: 'Курс вихідного дня Online',
-        en: 'Online weekend course',
-        link: 'online-weekend',
+        uk: "Дитячі групи",
+        en: "Kids groups",
+        link: "kids-group",
+      },
+      {
+        uk: "Курс вихідного дня Online",
+        en: "Online weekend course",
+        link: "online-group",
       },
     ],
   },
   {
     heading: {
-      uk: 'Інші мови',
-      en: 'Other Languages',
+      uk: "Інші мови",
+      en: "Other Languages",
     },
     items: [
       {
-        uk: 'Іспанська',
-        en: 'Spanish',
-        link: 'spanish',
+        uk: "Іспанська",
+        en: "Spanish",
+        link: "other-languages",
       },
       {
-        uk: 'Румунська',
-        en: 'Romanian',
-        link: 'romanian',
+        uk: "Румунська",
+        en: "Romanian",
+        link: "other-languages",
       },
       {
-        uk: 'Польська',
-        en: 'Polish',
-        link: 'polish',
+        uk: "Польська",
+        en: "Polish",
+        link: "other-languages",
       },
       {
-        uk: 'Французька',
-        en: 'French',
-        link: 'french',
+        uk: "Французька",
+        en: "French",
+        link: "other-languages",
       },
     ],
   },
   {
     heading: {
-      uk: 'Програма лояльності',
-      en: 'Loyalty program',
+      uk: "Інше",
+      en: "Other",
     },
     items: [
       {
-        uk: 'Тест онлайн',
-        en: 'Online test',
-        link: 'online-test',
+        uk: "Програма лояльності",
+        en: "Loyalty program",
+        link: "loyalty-program",
       },
       {
-        uk: 'Контакти',
-        en: 'Contacts',
-        link: 'contacts',
+        uk: "Тест онлайн",
+        en: "Online test",
+        link: "online-test",
+      },
+      {
+        uk: "Контакти",
+        en: "Contacts",
+        link: "contacts",
       },
     ],
   },
-]
+];
 
 export const privacy = {
-  uk: 'Політика конфіденційності',
-  en: 'Privacy Policy',
-  link: 'privacy',
-}
+  uk: "Політика конфіденційності",
+  en: "Privacy Policy",
+  link: "privacy",
+};
 
 export const memo = {
-  uk: 'створена у пам’ять про нашого друга і наставника Деніела Хейдена',
-  en: 'has been created in memory of our friend and mentor Daniel Hayden',
-}
+  uk: "створена у пам’ять про нашого друга і наставника Деніела Хейдена",
+  en: "has been created in memory of our friend and mentor Daniel Hayden",
+};
