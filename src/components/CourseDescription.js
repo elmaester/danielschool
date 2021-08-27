ce817631446fdc90183cf4502b4d90e82e09b66e
elmaester <olegmorket@gmail.com>
2021-08-27 11:54:24 +0300

    CourseDescription component

---
+++ src/components/CourseDescription.js
@@ -0,0 +1,68 @@
+import { GatsbyImage } from "gatsby-plugin-image";
+import * as React from "react";
+import styled from "styled-components";
+import GlobalContext from "../global-context";
+import { paddings, redColor, sizes } from "../responsive";
+import { descriptionWord } from "../text/courses/sharedCoursesText";
+
+const CourseDescriptionSC = styled.section`
+  padding: 42px ${paddings.phone}px 0;
+  ${sizes.tabletLandscape} {
+    padding-left: ${paddings.tabletLandscape}px;
+    padding-right: ${paddings.tabletLandscape}px;
+  }
+  ${sizes.desktop} {
+    padding: 70px ${paddings.desktop}px 0;
+  }
+  h2 {
+    display: inline-block;
+    padding: 0 20px 10px 0;
+    border-bottom: 1px solid ${redColor};
+  }
+`;
+
+const BelowTitleContainer = styled.div`
+  margin-top: 35px;
+  ${sizes.tablet} {
+    display: grid;
+    grid-template-columns: 1fr 300px;
+    gap: 20px;
+  }
+  ${sizes.tabletLandscape} {
+    grid-template-columns: 1fr 445px;
+  }
+`;
+
+const ParagraphsContainer = styled.div`
+  ${sizes.phone} {
+    margin-bottom: 20px;
+    p:not(:first-of-type) {
+      margin-top: 10px;
+    }
+  }
+  ${sizes.tablet} {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+`;
+
+const CourseDescription = ({ descriptionParagraphs, imageData, imageAlt }) => {
+  const { lang } = React.useContext(GlobalContext);
+  return (
+    <CourseDescriptionSC>
+      <h2>{descriptionWord[lang]}</h2>
+      <BelowTitleContainer>
+        <ParagraphsContainer>
+          {!!descriptionParagraphs.length &&
+            descriptionParagraphs.map((paragraph, index) => (
+              <p key={index}>{paragraph[lang]}</p>
+            ))}
+        </ParagraphsContainer>
+        <GatsbyImage image={imageData} alt={imageAlt[lang]} />
+      </BelowTitleContainer>
+    </CourseDescriptionSC>
+  );
+};
+
+export default CourseDescription;
--- src/pages/adults-group.js
+++ src/pages/adults-group.js
@@ -1,6 +1,7 @@
 import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 import CourseBanner from "../components/CourseBanner";
+import CourseDescription from "../components/CourseDescription";
 import CoursesCommonLayout from "../components/CoursesCommonLayout";
 import FeaturesList from "../components/FeaturesList";
 import GlobalContext from "../global-context";
@@ -9,7 +10,7 @@ import adultsCourseText from "../text/courses/adultsCourseText";
 const AdultsGroupPage = () => {
   const data = useStaticQuery(graphql`
     query AdultsGroupImageQuery {
-      file(base: { regex: "/601708961/" }) {
+      bannerImage: file(base: { regex: "/601708961/" }) {
         childImageSharp {
           gatsbyImageData(
             transformOptions: {
@@ -22,6 +23,17 @@ const AdultsGroupPage = () => {
         }
         base
       }
+      descriptionImage: file(base: { regex: "/1428145862/" }) {
+        childImageSharp {
+          gatsbyImageData(
+            transformOptions: { fit: COVER }
+            quality: 70
+            placeholder: BLURRED
+            width: 445
+          )
+        }
+        base
+      }
     }
   `);
   const { lang } = React.useContext(GlobalContext);
@@ -30,7 +42,12 @@ const AdultsGroupPage = () => {
       <CourseBanner
         bannerHeading={adultsCourseText.bannerHeading}
         bannerParagraphs={adultsCourseText.bannerParagraphs}
-        imageData={data.file.childImageSharp.gatsbyImageData}
+        imageData={data.bannerImage.childImageSharp.gatsbyImageData}
+      />
+      <CourseDescription
+        descriptionParagraphs={adultsCourseText.descriptionParagraphs}
+        imageData={data.descriptionImage.childImageSharp.gatsbyImageData}
+        imageAlt={adultsCourseText.bannerHeading}
       />
       <FeaturesList featuresList={adultsCourseText.featuresList} />
     </CoursesCommonLayout>
