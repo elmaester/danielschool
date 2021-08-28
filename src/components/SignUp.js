36bf8861675f46a4eec8ad56b7a55746975958f9
elmaester <olegmorket@gmail.com>
2021-08-28 11:50:37 +0300

    Form BG & style

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,6 +5,7 @@ import Multiselect from "multiselect-react-dropdown";
 import "./SignUp__InputStyles.css";
 import GlobalContext from "../global-context";
 import { blackColor, paddings, redColor, sizes } from "../responsive";
+import BackgroundImage from "gatsby-background-image";
 import {
   heading,
   languageChoice,
@@ -12,32 +13,54 @@ import {
   name,
   sendButton,
 } from "../text/signUpText";
+import { graphql, useStaticQuery } from "gatsby";
+import { convertToBgImage } from "gbimage-bridge";
 
-const SignUpSC = styled.section`
+const SignUpSC = styled(BackgroundImage)`
   color: white;
   background-color: ${blackColor};
   padding: 42px ${paddings.phone}px;
   text-align: center;
+  ${sizes.tablet} {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    h2 {
+      font-size: 40px;
+      margin-top: 50px;
+      text-align: left;
+      width: 50%;
+    }
+  }
+  ${sizes.tabletLandscape} {
+    padding-left: ${paddings.tabletLandscape}px;
+    padding-right: ${paddings.tabletLandscape}px;
+    h2 {
+      margin-top: 0;
+      font-size: 54px;
+    }
+  }
+  ${sizes.desktop} {
+    padding-left: ${paddings.desktop}px;
+    padding-right: ${paddings.desktop}px;
+  }
 `;
 
 const FormInputSC = styled.input`
   display: block;
   width: 100%;
   border: 2px solid white;
-  margin-top: 30px;
   padding: 10px 7px;
   box-sizing: border-box;
-  background-color: ${blackColor};
+  ${sizes.phone} {
+    margin-top: 30px;
+  }
+  background-color: transparent;
   color: white;
   outline: none;
   &::placeholder {
     color: white;
     font-size: 14px;
     line-height: 26px;
-  }
-  ${sizes.tablet} {
-    width: 360px;
-    margin-left: auto;
   }
 `;
 
@@ -54,13 +77,32 @@ const SendButtonSC = styled.button`
 `;
 
 const SignUp = () => {
+  const data = useStaticQuery(graphql`
+    query SignUpImageQuery {
+      file(base: { regex: "/639653137/" }) {
+        childImageSharp {
+          gatsbyImageData(
+            transformOptions: {
+              fit: COVER
+              duotone: { opacity: 40, shadow: "#040e1f", highlight: "#040e1f" }
+            }
+            quality: 70
+            placeholder: BLURRED
+          )
+        }
+        base
+      }
+    }
+  `);
   const { lang } = React.useContext(GlobalContext);
   const [clientName, setClientName] = React.useState("");
   const [number, setNumber] = React.useState("380");
   const [chosenLanguages, setChosenLanguages] = React.useState([]);
   return (
-    <SignUpSC>
-      <h2>{heading[lang]}</h2>
+    <SignUpSC {...convertToBgImage(data.file.childImageSharp.gatsbyImageData)}>
+      <div>
+        <h2>{heading[lang]}</h2>
+      </div>
       <aside>
         <FormInputSC
           type="text"
@@ -75,8 +117,12 @@ const SignUp = () => {
           closeIcon="cancel"
           closeOnSelect={false}
           hidePlaceholder
-          onSelect={(selectedList) => setChosenLanguages(selectedList.map((lang) => lang.en))}
-          onRemove={(selectedList) => setChosenLanguages(selectedList.map((lang) => lang.en))}
+          onSelect={(selectedList) =>
+            setChosenLanguages(selectedList.map((lang) => lang.en))
+          }
+          onRemove={(selectedList) =>
+            setChosenLanguages(selectedList.map((lang) => lang.en))
+          }
         />
         <PhoneInput
           country="ua"
