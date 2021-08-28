918b98040610d1312349165787f77c6ba37fb305
elmaester <olegmorket@gmail.com>
2021-08-28 15:17:37 +0300

    finish contacts component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,29 +1,96 @@
 import * as React from "react";
 import styled from "styled-components";
+import GoogleMapReact from "google-map-react";
 import GlobalContext from "../global-context";
-import { paddings } from "../responsive";
+import MapPin from "../images/map-pin.svg";
+import { paddings, sizes } from "../responsive";
 import { heading, location, phoneNuner } from "../text/contactsText";
 
 const ContactsSC = styled.section`
   padding: 0 ${paddings.phone}px;
   margin: 40px 0;
   text-align: center;
+  ${sizes.tablet} {
+    display: flex;
+    align-items: center;
+  }
+  ${sizes.tabletLandscape} {
+    padding-left: ${paddings.tabletLandscape}px;
+    padding-right: 0;
+  }
+  ${sizes.desktop} {
+    padding-left: ${paddings.desktop}px;
+  }
 `;
+
+const TextContainerSC = styled.div`
+  min-width: 300px;
+  ${sizes.tabletLandscape} {
+    min-width: 40%;
+  }
+  ${sizes.desktop} {
+    text-align: left;
+    min-width: 400px;
+  }
+`;
+
 const Heading = styled.h2`
   margin-bottom: 42px;
 `;
 const Phone = styled.h2`
   margin-bottom: 15px;
 `;
-const Location = styled.p``;
+const Location = styled.p`
+  &+& {
+    margin-top: 2px;
+  }
+`;
+
+const MapPinSC = styled.div`
+  height: 48px;
+  width: 48px;
+  transform: translate(-50%, -95%);
+`;
+
+const MapContainerSC = styled.div`
+  display: none;
+  ${sizes.tablet} {
+    display: block;
+    width: 100%;
+    height: 200px;
+  }
+  ${sizes.desktop} {
+    height: 295px;
+  }
+`;
 
 const Contacts = () => {
   const { lang } = React.useContext(GlobalContext);
+  const schoolCoords = { lat: 48.2857092, lng: 25.9402003 };
   return (
     <ContactsSC>
-      <Heading>{heading[lang]}</Heading>
-      <Phone>{phoneNuner}</Phone>
-      <Location>{location[lang]}</Location>
+      <TextContainerSC>
+        <Heading>{heading[lang]}</Heading>
+        <Phone>{phoneNuner}</Phone>
+        {location.map((line, index) => (
+          <Location key={index}>{line[lang]}</Location>
+        ))}
+      </TextContainerSC>
+      <MapContainerSC>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.GATSBY_MAPS,
+            language: lang,
+            region: lang,
+          }}
+          defaultCenter={schoolCoords}
+          defaultZoom={17}
+        >
+          <MapPinSC {...schoolCoords}>
+            <MapPin />
+          </MapPinSC>
+        </GoogleMapReact>
+      </MapContainerSC>
     </ContactsSC>
   );
 };
--- src/text/contactsText.js
+++ src/text/contactsText.js
@@ -1,11 +1,25 @@
 export const heading = {
-  uk: 'Контакти',
-  en: 'Contacts'
-}
+  uk: "Контакти",
+  en: "Contacts",
+};
 
-export const location = {
-  uk: 'м. Чернівці, Україна',
-  en: 'Chernivtsi, Ukraine'
-}
+export const location = [
+  {
+    uk: "Кімната 108,",
+    en: "Apt. 108,",
+  },
+  {
+    uk: "1 поверх,",
+    en: "1st floor,",
+  },
+  {
+    uk: "вул. Шевченка 28,",
+    en: "28 Shevchenka str.,",
+  },
+  {
+    uk: "м. Чернівці, Україна",
+    en: "Chernivtsi, Ukraine",
+  },
+];
 
-export const phoneNuner = '+38 (066) 743 11 37'
+export const phoneNuner = "+38 (066) 743 11 37";
