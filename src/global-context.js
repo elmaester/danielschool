9a41be93b2b667fa698e630e2305bdde78fddcbb
elmaester <olegmorket@gmail.com>
2021-08-29 14:56:15 +0300

    remember language choice and intelligently set default

--- src/global-context.js
+++ src/global-context.js
@@ -6,10 +6,24 @@ const GlobalContext = React.createContext();
 
 export const ContextProvider = ({ children }) => {
   const [lang, setLang] = React.useState("uk");
+  React.useEffect(() => {
+    const lsLang = JSON.parse(localStorage.getItem("lang"));
+    if (lsLang) {
+      setLang(lsLang);
+    } else {
+      if (
+        !navigator.languages.some((lng) => lng.includes("uk")) &&
+        !navigator.languages.some((lng) => lng.includes("ru"))
+      ) {
+        setLang("en");
+      }
+    }
+  }, []);
 
   const setLanguage = (language) => {
     if (langs.includes(language)) {
       setLang(language);
+      localStorage.setItem("lang", JSON.stringify(language));
     }
   };
   return (
