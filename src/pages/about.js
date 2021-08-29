c24524d93ff57fbda8ae8f7cfa770b6ab82ed21b
elmaester <olegmorket@gmail.com>
2021-08-29 14:15:49 +0300

    add signup to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,13 @@ import AboutUs from "../components/AboutUs";
 import Footer from "../components/Footer";
 import GlobalStyle from "../components/GlobalStyle";
 import Header from "../components/Header";
+import SignUp from "../components/SignUp";
 
 const AboutUsPage = () => (
   <GlobalStyle>
     <Header />
     <AboutUs />
+    <SignUp />
     <Footer />
   </GlobalStyle>
 );
